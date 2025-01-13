cf13716de7225841b4982b9c56a1470e2d675333
fitchatron <53107122+fitchatron@users.noreply.github.com>
2025-01-13 11:24:24 +1100

    user docs

--- backend/src/api/v1/users.ts
+++ backend/src/api/v1/users.ts
@@ -111,6 +111,35 @@ app.post("/", async (req: Request, res: Response) => {
   return;
 });
 
+/**
+ * @openapi
+ * /api/v1/users/{userId}:
+ *   get:
+ *     summary: Get a user
+ *     description: Get a user by ID
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: User ID
+ *     responses:
+ *       201:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *              $ref: '#/components/schemas/User'
+ *       401:
+ *         $ref: '#/components/responses/Unauthorized'
+ *       403:
+ *         $ref: '#/components/responses/Forbidden'
+ *       404:
+ *         $ref: '#/components/responses/NotFound'
+ *
+ */
 app.get("/:userId", async (req: Request, res: Response) => {
   try {
     const userId = req.params.userId;
@@ -130,6 +159,60 @@ app.get("/:userId", async (req: Request, res: Response) => {
   }
 });
 
+/**
+ * @openapi
+ * /api/v1/users/{userId}:
+ *   put:
+ *     summary: Update a user
+ *     description: Update a user
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: User ID
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - firstName
+ *               - lastName
+ *               - email
+ *             properties:
+ *               firstName:
+ *                 type: string
+ *               lastName:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 description: must be unique
+ *             example:
+ *               firstName: John
+ *               lastName: Doe
+ *               email: john.doe@example.com
+ *     responses:
+ *       201:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *              $ref: '#/components/schemas/User'
+ *       400:
+ *         $ref: '#/components/responses/DuplicateEmail'
+ *       401:
+ *         $ref: '#/components/responses/Unauthorized'
+ *       403:
+ *         $ref: '#/components/responses/Forbidden'
+ *       404:
+ *         $ref: '#/components/responses/NotFound'
+ *
+ */
 app.put("/:userId", async (req: Request, res: Response) => {
   try {
     const userId = req.params.userId;
@@ -147,6 +230,32 @@ app.put("/:userId", async (req: Request, res: Response) => {
   }
 });
 
+/**
+ * @openapi
+ * /api/v1/users/{userId}:
+ *   delete:
+ *     summary: Delete a user
+ *     description: Logged in users can delete only themselves. Only admins can delete other users.
+ *     tags: [Users]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: UserId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: User id
+ *     responses:
+ *       "200":
+ *         description: Number of rows deleted
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ *       "404":
+ *         $ref: '#/components/responses/NotFound'
+ */
 app.delete("/:userId", async (req: Request, res: Response) => {
   try {
     const userId = req.params.userId;
@@ -164,6 +273,63 @@ app.delete("/:userId", async (req: Request, res: Response) => {
   }
 });
 
+/**
+ *
+ * @openapi
+ * /api/v1/users/{userId}/roles:
+ *   post:
+ *     summary: Create a new user role
+ *     description: Create a new user role for the given user
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: User id
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - userId
+ *               - roleId
+ *               - createdBy
+ *               - modifiedBy
+ *             properties:
+ *               userId:
+ *                 type: string
+ *               roleId:
+ *                 type: string
+ *               createdBy:
+ *                 type: string
+ *                 format: date-time
+ *               modifiedBy:
+ *                 type: string
+ *                 format: date-time
+ *             example:
+ *               userId: 14c5f260-2ae0-49b7-9968-f6ed2e082526
+ *               roleId: 14c5f260-2ae0-49b7-9968-f6ed2e082526
+ *               createdBy: 14c5f260-2ae0-49b7-9968-f6ed2e082526
+ *               modifiedBy: 14c5f260-2ae0-49b7-9968-f6ed2e082526
+ *     responses:
+ *       201:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *              $ref: '#/components/schemas/User'
+ *       400:
+ *         $ref: '#/components/responses/DuplicateEntity'
+ *       401:
+ *         $ref: '#/components/responses/Unauthorized'
+ *       403:
+ *         $ref: '#/components/responses/Forbidden'
+ *
+ */
 app.post("/:userId/roles", async (req: Request, res: Response) => {
   try {
     const roleId = req.params.roleId;
@@ -182,6 +348,38 @@ app.post("/:userId/roles", async (req: Request, res: Response) => {
   }
 });
 
+/**
+ * @openapi
+ * /api/v1/users/{userId}/roles/{roleId}:
+ *   delete:
+ *     summary: Delete a user role
+ *     description: Deletes a user role given a user ID and role ID
+ *     tags: [Users]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: User id
+ *       - in: path
+ *         name: roleId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Role id
+ *     responses:
+ *       "200":
+ *         description: Number of rows deleted
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ *       "404":
+ *         $ref: '#/components/responses/NotFound'
+ */
 app.delete("/:userId/roles/:roleId", async (req: Request, res: Response) => {
   try {
     const userId = req.params.userId;
