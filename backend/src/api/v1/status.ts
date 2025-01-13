4b3b98034e612a5ac2466a556511300e60e7042e
fitchatron <53107122+fitchatron@users.noreply.github.com>
2025-01-13 11:24:29 +1100

    status docs

--- backend/src/api/v1/status.ts
+++ backend/src/api/v1/status.ts
@@ -1,10 +1,41 @@
 import express, { Request, Response } from "express";
 
 const app = express();
+/**
+ * @swagger
+ * tags:
+ *   name: Status
+ *   description: Server status
+ */
 
+/**
+ * @openapi
+ * /api/v1/status:
+ *   get:
+ *     summary: Check if server is running
+ *     description: Get a response if servier is up.
+ *     tags: [Status]
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *              type: object
+ *              properties:
+ *                status:
+ *                  type: string
+ *                runAt:
+ *                  type: string
+ *                  format: date-time
+ *             example:
+ *               status: Running
+ *               runAt: "2025-01-10T03:30:22.440Z"
+ *
+ */
 app.get("/", (req: Request, res: Response) => {
   res.status(200).send({
-    status: "v1 running",
+    status: "Running",
     runAt: new Date().toISOString(),
   });
   return;
