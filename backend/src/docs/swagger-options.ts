316cb227aedbfbb908ec6e5cf046f9fe8130cac0
fitchatron <53107122+fitchatron@users.noreply.github.com>
2025-01-14 12:40:45 +1100

    docs config

--- backend/src/docs/swagger-options.ts
+++ backend/src/docs/swagger-options.ts
@@ -13,6 +13,15 @@ const swaggerOptions: Options = {
         url: "http://localhost:8000",
       },
     ],
+    tags: [
+      {
+        name: "Permissions",
+        description: "Permission management and retrieval",
+      },
+      { name: "Roles", description: "Role management and retrieval" },
+      { name: "Status", description: "Server status" },
+      { name: "Users", description: "User management and retrieval" },
+    ],
   },
   apis: ["src/docs/*.yml", "src/api/v1/*.ts"], // Path to the API docs in your routes
 };
