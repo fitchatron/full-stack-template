5461eec369a300c09070802f2f6531fce92288dc
fitchatron <53107122+fitchatron@users.noreply.github.com>
2026-09-07 09:22:40 +1000

    init fastapi backend
    fastapi
    uv
    ruff

---
+++ backend/README.md
@@ -0,0 +1,3 @@
+# Backend App
+
+This directory contains the backend application for the Full-Stack Template project. It includes all server-side code, configurations, and dependencies required to run the backend service.
--- backend/src/db/schema/resources.ts
+++
@@ -1,9 +0,0 @@
-import { jsonb, pgTable, varchar } from "drizzle-orm/pg-core";
-import { timestamps, userMetadata } from "@db/columns.helpers";
-
-export const resources = pgTable("resources", {
-  id: varchar().notNull().primaryKey(),
-  mappings: jsonb().notNull(),
-  ...timestamps,
-  ...userMetadata,
-});
